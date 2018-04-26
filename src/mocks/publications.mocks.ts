c42ae1913d6a5cb48a8b5c65360b605a90595cdf
Cristian Camilo Londoño <cricalonos@gmail.com>
2018-04-26 09:52:35 -0500

    Ajustes
    Se realizan ajustes en el header del home, los datos de los mocks y la cámara de la página camera.

--- src/mocks/publications.mocks.ts
+++ src/mocks/publications.mocks.ts
@@ -8,32 +8,32 @@ export let publications = [
     }, {
         "profile": "assets/imgs/fotos-perfil/perfil2.png",
         "user": "N.N.",
-        "location": "Medellón, Antioquia",
+        "location": "Medellín, Antioquia",
         "picture": "assets/imgs/img2.jpeg",
-        "comment": "Comentario de la foto de los rieles"
+        "comment": "Las gafas, también conocidas como lentes, anteojos, antiparras, binóculos o espejuelos, son un instrumento óptico ..."
     }, {
         "profile": "assets/imgs/fotos-perfil/perfil3.jpg",
         "user": "Homero J. Simpson",
         "location": "Springfield",
         "picture": "assets/imgs/img3.jpeg",
-        "comment": "Comentario de la foto de los rieles"
+        "comment": "Las arecáceas, son una familia de plantas monocotiledóneas, la única familia del orden Arecales. Normalmente se las conoce como palmeras o palmas."
     }, {
         "profile": "assets/imgs/fotos-perfil/perfil4.jpg",
         "user": "Chaozu",
         "location": "Planeta namekusei",
         "picture": "assets/imgs/img4.jpg",
-        "comment": "Comentario de la foto de los rieles"
+        "comment": "Llamado comúnmente girasol, calom, jáquima, maravilla, mirasol, tlapololote, maíz de teja, acahual2​ o flor de escudo."
     }, {
         "profile": "assets/imgs/fotos-perfil/perfil5.jpg",
         "user": "Botas",
         "location": "Algún lugar del mundo",
         "picture": "assets/imgs/img5.jpg",
-        "comment": "Comentario de la foto de los rieles"
+        "comment": "Es una especie de mamífero del orden de los carnívoros y aunque hay una gran controversia al respecto, los últimos estudios de su ADN lo engloban entre los miembros de la familia de los osos..."
     }, {
         "profile": "assets/imgs/fotos-perfil/perfil1.png",
         "user": "Naruto",
         "location": "Calarca, Quindío",
         "picture": "assets/imgs/img6.jpg",
-        "comment": "Comentario de la foto de los rieles"
+        "comment": "Un lago es un cuerpo de agua, generalmente dulce, de una extensión considerable, que se encuentra separado del mar."
     },
 ]
